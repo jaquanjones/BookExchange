cfc07fce9ea298686fc88145e89d27aafa58370b
Jaquan Jones <jaquan.dev@gmail.com>
2020-11-15 15:25:06 -0800

    - fixed post book screen - plan adding set up search page/functionality - plan adding favorite functionality

--- bookMng/forms.py
+++ bookMng/forms.py
@@ -12,3 +12,25 @@ class BookForm(ModelForm):
             'price',
             'picture',
         ]
+        widgets = {
+            'name': forms.TextInput(
+                attrs={
+                    'class': 'form-control'
+                }
+            ),
+            'web': forms.TextInput(
+                attrs={
+                    'class': 'form-control'
+                }
+            ),
+            'price': forms.TextInput(
+                attrs={
+                    'class': 'form-control'
+                }
+            ),
+            'picture': forms.FileInput(attrs={
+                'class': 'custom-file-input',
+                'id': 'image-upload',
+                'required': True
+            }),
+        }
