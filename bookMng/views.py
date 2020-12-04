f25474d657b6447ae06ddc8885757251c540db29
tommylay1903@gmail.com <tommylay1903@gmail.com>
2020-12-03 22:41:56 -0800

    implemented pagination in display and my books. Only error is styling

--- bookMng/views.py
+++ bookMng/views.py
@@ -16,7 +16,67 @@ from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
+# Create your views here.
+def index(request):
+    # return HttpResponse("<h1 align='center'>Hello World</h1>")
+    # return render(request, 'base.html')
+    # return render(request, 'bookMng/displaybooks.html')
+    return render(request,
+                  'bookMng/home.html',
+                  {
+                      'item_list': MainMenu.objects.all()
+                  }
+                  )
+
+
+@login_required(login_url=reverse_lazy('login'))
+def postbook(request):
+    submitted = False
+    if request.method == 'POST':
+        form = BookForm(request.POST, request.FILES)
+        if form.is_valid():
+            # form.save()
+            book = form.save(commit=False)
+            try:
+                book.username = request.user
+            except Exception:
+                pass
+            book.save()
+            return HttpResponseRedirect('/postbook?submitted=True')
+    else:
+        form = BookForm()
+        if 'submitted' in request.GET:
+            submitted = True
+    return render(request,
+                  'bookMng/postbook.html',
+                  {
+                      'form': form,
+                      'item_list': MainMenu.objects.all(),
+                      'submitted': submitted
+                  })
+
+
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from .models import MainMenu
+
+from .forms import BookForm
+from django.http import HttpResponseRedirect
+
+from .models import Book
+
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+
+from django.contrib.auth.decorators import login_required
+
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from .filters import BookFilter
 
 # Create your views here.
 def index(request):
@@ -62,17 +122,35 @@ def postbook(request):
 def displaybooks(request):
     books = Book.objects.all()
 
+    #data gets rendered and filters it down if there are any filters applied
     myFilter = BookFilter(request.GET, queryset=books)
-
+    #rebuilds the result from the filter
     books = myFilter.qs
+
+    # pagination for display books, change number of second parameter to get a customized number of books per page
+    paginator = Paginator(books, 5)
+
+    # will grab the current page from the url
+    page = request.GET.get('page')
+
+    books = paginator.get_page(page)
+
     for b in books:
         b.pic_path = b.picture.url[14:]
+
+    try:
+        response = paginator.page('page')
+    except PageNotAnInteger:
+        response = paginator.page(1)
+    except EmptyPage:
+        response = paginator.page(paginator.num_pages)
     return render(request,
                   'bookMng/displaybooks.html',
                   {
                       'item_list': MainMenu.objects.all(),
                       'books': books,
-                      'filters': myFilter
+                      'filters': myFilter,
+                      'response': response
                   })
 
 
